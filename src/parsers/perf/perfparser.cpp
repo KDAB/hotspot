d666ecb8388860286f0cbe8805cc68730aaea042
Milian Wolff <milian.wolff@kdab.com>
2016-12-21 00:10:05 +0100

    Add basic parsing of perfparser output.

--- src/parsers/perf/perfparser.cpp
+++ src/parsers/perf/perfparser.cpp
@@ -29,8 +29,128 @@
 
 #include <QProcess>
 #include <QDebug>
+#include <QtEndian>
+#include <QBuffer>
+#include <QDataStream>
 
 #include "util.h"
+
+namespace {
+struct ParserData
+{
+    ParserData()
+    {
+        buffer.buffer().reserve(1024);
+        buffer.open(QIODevice::ReadOnly);
+        stream.setDevice(&buffer);
+    }
+    bool tryParse (QProcess *process)
+    {
+        const auto bytesAvailable = process->bytesAvailable();
+        switch (state) {
+            case HEADER: {
+                const auto magic = QLatin1String("QPERFSTREAM");
+                // + 1 to include the trailing \0
+                if (bytesAvailable >= magic.size() + 1) {
+                    process->read(buffer.buffer().data(), magic.size() + 1);
+                    if (buffer.buffer().data() != magic) {
+                        state = PARSE_ERROR;
+                        qWarning() << "Failed to read header magic";
+                        return false;
+                    } else {
+                        state = DATA_STREAM_VERSION;
+                        return true;
+                    }
+                }
+                break;
+            }
+            case DATA_STREAM_VERSION: {
+                qint32 dataStreamVersion = 0;
+                if (bytesAvailable >= sizeof(dataStreamVersion)) {
+                    process->read(buffer.buffer().data(), sizeof(dataStreamVersion));
+                    dataStreamVersion = qFromLittleEndian(*reinterpret_cast<qint32*>(buffer.buffer().data()));
+                    stream.setVersion(dataStreamVersion);
+                    qDebug() << "data stream version is:" << dataStreamVersion;
+                    state = EVENT_HEADER;
+                    return true;
+                }
+                break;
+            }
+            case EVENT_HEADER:
+                if (bytesAvailable >= sizeof(eventSize)) {
+                    process->read(buffer.buffer().data(), sizeof(eventSize));
+                    eventSize = qFromLittleEndian(*reinterpret_cast<quint32*>(buffer.buffer().data()));
+                    qDebug() << "next event size is:" << eventSize;
+                    state = EVENT;
+                    return true;
+                }
+            case EVENT:
+                if (bytesAvailable >= eventSize) {
+                    buffer.buffer().resize(eventSize);
+                    process->read(buffer.buffer().data(), eventSize);
+                    if (!parseEvent()) {
+                        state = PARSE_ERROR;
+                        return false;
+                    }
+                    // await next event
+                    state = EVENT_HEADER;
+                    return true;
+                }
+            case PARSE_ERROR:
+                // do nothing
+                break;
+        }
+        return false;
+    }
+
+    bool parseEvent()
+    {
+        Q_ASSERT(buffer.isOpen());
+        Q_ASSERT(buffer.isReadable());
+
+        buffer.seek(0);
+        Q_ASSERT(buffer.pos() == 0);
+
+        stream.resetStatus();
+
+        qint8 eventType = 0;
+        stream >> eventType;
+        qDebug() << "next event is:" << eventType;
+
+        if (eventType < 0 || eventType >= InvalidType) {
+            state = PARSE_ERROR;
+            return false;
+        }
+
+        return true;
+    }
+
+    enum State {
+        HEADER,
+        DATA_STREAM_VERSION,
+        EVENT_HEADER,
+        EVENT,
+        PARSE_ERROR
+    };
+
+    enum EventType {
+        Sample,
+        ThreadStart,
+        ThreadEnd,
+        Command,
+        LocationDefinition,
+        SymbolDefinition,
+        AttributesDefinition,
+        InvalidType
+    };
+
+    State state = HEADER;
+    quint32 eventSize = 0;
+    QBuffer buffer;
+    QDataStream stream;
+};
+
+}
 
 PerfParser::PerfParser(QObject* parent)
     : QObject(parent)
@@ -45,10 +165,19 @@ bool PerfParser::parseFile(const QString& path)
     const auto parser = Util::findLibexecBinary(QStringLiteral("hotspot-perfparser"));
     if (parser.isEmpty()) {
         qWarning() << "Failed to find hotspot-perfparser binary.";
-        return {false};
+        return false;
     }
 
     QProcess parserProcess;
+
+    ParserData data;
+    connect(&parserProcess, &QProcess::readyRead,
+            [&data, &parserProcess] {
+                while (data.tryParse(&parserProcess)) {
+                    // just call tryParse until it fails
+                }
+            });
+
     parserProcess.start(parser, {QStringLiteral("--input"), path});
     if (!parserProcess.waitForStarted()) {
         qWarning() << "Failed to start hotspot-perfparser binary" << parser;
@@ -60,5 +189,6 @@ bool PerfParser::parseFile(const QString& path)
         return false;
     }
 
+    Q_ASSERT(data.state == ParserData::PARSE_ERROR || !parserProcess.bytesAvailable());
     return true;
 }
