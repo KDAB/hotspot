8ff9543b754ea437a25d9f6d024576a774f00591
Milian Wolff <milian.wolff@kdab.com>
2017-02-17 18:41:08 +0100

    Increase load of cpp-inlining example
    This way it generates more samples and produces
    more meaningful test data.

--- tests/test-clients/cpp-inlining/main.cpp
+++ tests/test-clients/cpp-inlining/main.cpp
@@ -10,7 +10,7 @@ int main()
     uniform_real_distribution<double> uniform(-1E5, 1E5);
     default_random_engine engine;
     double s = 0;
-    for (int i = 0; i < 1000000; ++i) {
+    for (int i = 0; i < 10000000; ++i) {
         s += norm(complex<double>(uniform(engine), uniform(engine)));
     }
     cout << s << '\n';
