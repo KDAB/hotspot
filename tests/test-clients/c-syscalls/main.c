e025fc79e70e441f58581d5bcf41b2a6cec2921f
Milian Wolff <milian.wolff@kdab.com>
2017-09-04 18:38:09 +0200

    Add a test client that stresses syscalls from C
    Here, unwinding seems to work fine. But note how perf record by
    default excludes kernel space nowadays. So better record via
    
        perf record -e cycles:P -c 1000000 --call-graph dwarf

---
+++ tests/test-clients/c-syscalls/main.c
@@ -0,0 +1,49 @@
+/*
+  This file is part of Hotspot, the Qt GUI for performance analysis.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Milian Wolff <milian.wolff@kdab.com>
+
+  Licensees holding valid commercial KDAB Hotspot licenses may use this file in
+  accordance with Hotspot Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    FILE* file;
+    long fileSize;
+    char* buffer;
+
+    for (int i = 0; i < 500000; ++i) {
+        file = fopen(argv[0], "rb");
+        if (!file) {
+            fprintf(stderr, "failed to open file %s\n", argv[0]);
+            return 1;
+        }
+
+        fseek(file, 0, SEEK_END);
+        fileSize = ftell(file);
+        rewind(file);
+
+        fclose(file);
+    }
+    return 0;
+}
