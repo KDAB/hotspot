26fb9197bf58e812ded394b9d174083bd5a1fa26
Milian Wolff <milian.wolff@kdab.com>
2017-09-04 18:28:19 +0200

    Add cpp-locking test client
    This example has high syscall overhead and excessive lock contention.
    It actually shows quite some issues when we try to unwind from the
    syscall boundary, which also affects perf report though:
    
        19.91%  cpp-locking  [kernel.vmlinux]     [.] syscall_return_via_sysret
                |
                ---syscall_return_via_sysret
    
        17.85%  cpp-locking  [kernel.vmlinux]     [.] entry_SYSCALL_64_fastpath
                |
                ---entry_SYSCALL_64_fastpath
    
         6.53%  cpp-locking  [kernel.vmlinux]     [.] native_irq_return_iret
                |
                ---native_irq_return_iret

---
+++ tests/test-clients/cpp-locking/main.cpp
@@ -0,0 +1,59 @@
+/*
+  This file is part of Hotspot, the Qt GUI for performance analysis.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Milian Wolff <milian.wolff@kdab.com>
+
+  Licensees holding valid commercial KDAB Hotspot licenses may use this file in
+  accordance with Hotspot Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <complex>
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <future>
+
+using namespace std;
+
+mutex g_mutex;
+
+double worker()
+{
+    lock_guard<mutex> guard(g_mutex);
+    uniform_real_distribution<double> uniform(-1E5, 1E5);
+    default_random_engine engine;
+    double s = 0;
+    for (int i = 0; i < 1000; ++i) {
+        s += norm(complex<double>(uniform(engine), uniform(engine)));
+    }
+    cout << s << endl;
+    return s;
+}
+
+int main()
+{
+    vector<std::future<double>> results;
+    for (int i = 0; i < 10000; ++i) {
+        results.push_back(async(launch::async, worker));
+    }
+    return 0;
+}
