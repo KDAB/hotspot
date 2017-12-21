cb86024fd3642083936ffc45634aff805d641a05
Milian Wolff <milian.wolff@kdab.com>
2017-12-21 16:37:24 +0100

    Add a simple executable that dumps a perf.data file to text
    This is mostly useful for debugging purposes, such that we can
    see what hotspot will see in text form. I.e. we can grep and diff
    this easily.

---
+++ tests/integrationtests/dump_perf_data.cpp
@@ -0,0 +1,74 @@
+/*
+  dump_perf_data.cpp
+
+  This file is part of Hotspot, the Qt GUI for performance analysis.
+
+  Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Nate Rogers <nate.rogers@kdab.com>
+
+  Licensees holding valid commercial KDAB Hotspot licenses may use this file in
+  accordance with Hotspot Commercial License Agreement provided with the Software.
+
+  Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QCoreApplication>
+#include <QDebug>
+
+#include "perfparser.h"
+#include "../testutils.h"
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+
+    auto args = app.arguments();
+    args.removeFirst();
+    if (args.isEmpty()) {
+        qWarning("missing perf.data file path argument");
+        return 1;
+    }
+
+    qRegisterMetaType<Data::BottomUpResults>();
+    qRegisterMetaType<Data::EventResults>();
+
+    int runningParsers = 0;
+    for (const auto& arg : args) {
+        auto parser = new PerfParser(&app);
+        parser->startParseFile(arg, {}, {}, {}, {}, {}, {});
+        ++runningParsers;
+        QObject::connect(parser, &PerfParser::parsingFinished,
+                         parser, [&runningParsers, &app]() {
+                            --runningParsers;
+                            if (!runningParsers)
+                                app.quit();
+                        });
+        QObject::connect(parser, &PerfParser::parsingFailed,
+                         parser, [&runningParsers, &app](const QString& error) {
+                            qWarning() << error;
+                            --runningParsers;
+                            if (!runningParsers)
+                                app.quit();
+                        });
+        QObject::connect(parser, &PerfParser::bottomUpDataAvailable,
+                         parser, [arg](const Data::BottomUpResults& data) {
+                            qDebug() << arg;
+                            dumpList(printTree(data));
+                        });
+    }
+
+    return app.exec();
+}
