58da1c7a7607baa2cdbae7328aeacf0af4368fa2
Milian Wolff <milian.wolff@kdab.com>
2019-08-09 11:25:50 +0200

    update perfparser and build perf2text helper

--- 3rdparty/perfparser.cmake
+++ 3rdparty/perfparser.cmake
@@ -63,15 +63,25 @@ ecm_add_test(
         tst_addresscache
 )
 
-ecm_add_test(
-    perfparser/tests/auto/kallsyms/tst_kallsyms.cpp
-    perfparser/app/perfkallsyms.cpp
-    LINK_LIBRARIES
-        Qt5::Core
-        Qt5::Network
-        Qt5::Test
-        ${LIBDW_LIBRARIES}
-        ${LIBELF_LIBRARIES}
-    TEST_NAME
-        tst_kallsyms
+include_directories(perfparser/tests/auto/shared)
+add_executable(perf2text
+    perfparser/tests/manual/perf2text/perf2text.cpp
+    perfparser/tests/auto/shared/perfparsertestclient.cpp
+)
+target_compile_definitions(perf2text
+    PRIVATE MANUAL_TEST
+)
+target_link_libraries(perf2text
+    Qt5::Core
+)
+
+set_target_properties(perf2text
+    PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/${KDE_INSTALL_BINDIR}"
+)
+
+add_custom_target(link_perfparser ALL
+    COMMAND ${CMAKE_COMMAND} -E create_symlink
+        "${PROJECT_BINARY_DIR}/${KDE_INSTALL_LIBEXECDIR}/hotspot-perfparser"
+        "${PROJECT_BINARY_DIR}/${KDE_INSTALL_BINDIR}/perfparser"
 )
