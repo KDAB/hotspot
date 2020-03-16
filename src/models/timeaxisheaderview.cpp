555d09d885ffa2e54e7c0b0369b441b5e6b8dae4
Koen Poppe <18189441+koenpoppe@users.noreply.github.com>
2020-03-16 22:40:23 +0100

    Move helper class PrefixTickLabels to anonymous namespace in cpp file

--- src/models/timeaxisheaderview.cpp
+++ src/models/timeaxisheaderview.cpp
@@ -7,6 +7,65 @@
 #include "filterandzoomstack.h"
 #include "eventmodel.h"
 
+namespace
+{
+class PrefixTickLabels
+{
+    // Construction
+public:
+    /**
+     * @param min, lower limit of the range
+     * @param max, upper limit of the range
+     * @param targetNbTicks,
+     * @throws std::invalid_argument, min > max
+     */
+    PrefixTickLabels(double min, double max, unsigned targetNbTicks);
+private:
+    static const int s_group_10 = 3; // Scientific notation is multiple of 3
+
+
+    // Prefix
+public:
+    /** Is there a common value that can be used as prefix?
+     */
+    bool hasPrefix() const;
+    /**
+     * @pre hasPrefix()
+     */
+    double prefixValue() const;
+    /** Textual representation of the prefix value
+     * @pre hasPrefix()
+     * @param placeholder, character used to indicate place of the ticks values
+     */
+    QString prefixLabel(const QString &placeholder = QLatin1String("xxx")) const;
+private:
+    int m_prefix_10 = 0;
+    double m_prefixValue = 0.0;
+    QString m_prefixLabel;
+
+
+    // Ticks
+public:
+    using TicksAndLabel = QVector<QPair<double, QString>>;
+    const TicksAndLabel &ticksAndLabel() const;
+private:
+    TicksAndLabel m_ticksAndLabels;
+
+
+    // Utility
+public:
+    /** Returns a power of 10 multiple of 1, 2 or 5 for the given value
+     * @see "Nice Numbers for Graph Labels", Graphics Gems, Andrew S. Glassner, Elsevier Science, 1990
+     */
+    static double niceNum(double value);
+    /** Prefix for the given power of 10
+     * @param power
+     * @throws std::invalid_argument, if there is no SI prefix for the given power
+     */
+    static QString siPrefix(int power);
+};
+}
+
 TimeAxisHeaderView::TimeAxisHeaderView(FilterAndZoomStack *filterAndZoomStack, Qt::Orientation orientation, QWidget *parent)
     : QHeaderView(orientation, parent)
     , m_filterAndZoomStack(filterAndZoomStack)
@@ -96,7 +155,7 @@ void TimeAxisHeaderView::paintSection(QPainter *painter, const QRect &rect, int 
         }
     }
 }
-    
+
 
 // Construction
 
