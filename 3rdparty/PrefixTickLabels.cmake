7dd1edec53a562c0c78ff0c9b1dd32f478c174b5
Koen Poppe <18189441+koenpoppe@users.noreply.github.com>
2020-05-21 09:06:24 +0200

    Clean up cmake file

--- 3rdparty/PrefixTickLabels.cmake
+++ 3rdparty/PrefixTickLabels.cmake
@@ -1,14 +1,9 @@
 add_library(PrefixTickLabels STATIC
     PrefixTickLabels/src/PrefixTickLabels.cpp
     )
-include_directories(
-    PrefixTickLabels/src
-    )
 target_include_directories(PrefixTickLabels PUBLIC
     PrefixTickLabels/src
     )
-target_link_libraries(PrefixTickLabels
+target_link_libraries(PrefixTickLabels PUBLIC
     Qt5::Core
     )
-
-
