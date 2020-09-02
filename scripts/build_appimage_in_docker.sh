218765af5485efdb90c35cf9a0114d51f05f9bf7
Milian Wolff <milian.wolff@kdab.com>
2020-09-02 09:42:22 +0200

    Allow disabling rustc demangling support in AppImage
    This is required for compatibility with CentOS 7, as librustc_demangle
    seems to pull in a GLIBC_2.18 symbol that's not available on that
    platform.
    
    Relates-To: https://github.com/KDAB/hotspot/issues/250

--- scripts/build_appimage_in_docker.sh
+++ scripts/build_appimage_in_docker.sh
@@ -15,14 +15,16 @@ if [ ! -e "$elfutils_package" ]; then
     exit 1
 fi
 
-if [ ! -e "$rustc_demangle_package" ]; then
+if [ -z "$DISABLE_RUSTC_DEMANGLE" ] && [ ! -e "$rustc_demangle_package" ]; then
     echo "missing \"$rustc_demangle_package\" - did you run package_rustc_demangle.sh already?"
     exit 1
 fi
 
 cd ..
 
-sudo docker build -t hotspot_appimage -f scripts/Dockerfile . || exit 1
+echo "RUSTC DISABLED? $DISABLE_RUSTC_DEMANGLE"
+
+sudo docker build --build-arg DISABLE_RUSTC_DEMANGLE="$DISABLE_RUSTC_DEMANGLE" -t hotspot_appimage -f scripts/Dockerfile . || exit 1
 sudo docker run -v "$artifacts":/artifacts -it hotspot_appimage
 mv "$artifacts"/hotspot-x86_64.AppImage hotspot-$(git describe)-x86_64.AppImage
 ls -latr hotspot-*.AppImage | tail -n 1
