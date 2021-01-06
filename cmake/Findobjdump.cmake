db70aa516b92493d15282d06785f127df0efd09a
Milian Wolff <milian.wolff@kdab.com>
2021-01-06 17:33:26 +0100

    Cleanup Findobjdump.cmake

--- cmake/Findobjdump.cmake
+++ cmake/Findobjdump.cmake
@@ -1,4 +1,17 @@
-find_program(objdump QUIET)
-set_package_properties(objdump PROPERTIES
-        DESCRIPTION "Utility to generate disassembler"
-        TYPE RUNTIME)
+if (OBJDUMP_PROGRAM)
+  set (objdump_FIND_QUIETLY TRUE)
+endif()
+
+find_program(OBJDUMP_PROGRAM
+    NAMES
+        objdump
+    HINTS
+        ${CMAKE_OBJDUMP})
+
+include (FindPackageHandleStandardArgs)
+
+# handle the QUIETLY and REQUIRED arguments and set objdump_FOUND to TRUE if all listed variables are TRUE
+find_package_handle_standard_args(objdump DEFAULT_MSG
+    OBJDUMP_PROGRAM)
+
+mark_as_advanced(OBJDUMP_PROGRAM)
