65124ed7a7b7b29ec5a06baa3dfabd93e4cfb377
Lieven Hey <lieven.hey@kdab.com>
2022-01-31 12:23:06 +0100

    fix tst_models
    DisassemblyModel got two additional columns causing the test to fail

--- tests/modeltests/tst_models.cpp
+++ tests/modeltests/tst_models.cpp
@@ -297,7 +297,7 @@ private slots:
         DisassemblyModel model;
         QAbstractItemModelTester tester(&model);
         model.setResults(results);
-        QCOMPARE(model.columnCount(), 1 + results.selfCosts.numTypes());
+        QCOMPARE(model.columnCount(), 3 + results.selfCosts.numTypes()); // disassembly + linked funtion
         QCOMPARE(model.rowCount(), 0); // no disassembly data yet
 
         DisassemblyOutput disassemblyOutput = DisassemblyOutput::disassemble("objdump","x86_64", symbol);
