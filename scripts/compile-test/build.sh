7e87c74e59e3485f1c2d525f4b374f00df12161c
Milian Wolff <milian.wolff@kdab.com>
2022-06-26 01:03:14 +0200

    Add CI coverage for Ubuntu 22.04
    this is more modern and allows us to use the cmake presets feature.

--- scripts/compile-test/build.sh
+++ scripts/compile-test/build.sh
@@ -4,7 +4,8 @@ set -e
 
 cd $(dirname $0)/../../
 
-docker build -t hotspot-build-ubuntu -f scripts/compile-test/Ubuntu .
+docker build -t hotspot-build-ubuntu20.04 -f scripts/compile-test/Ubuntu20.04 .
+docker build -t hotspot-build-ubuntu22.04 -f scripts/compile-test/Ubuntu22.04 .
 docker build -t hotspot-build-fedora -f scripts/compile-test/Fedora .
 docker build -t hotspot-build-archlinux -f scripts/compile-test/Archlinux .
 docker build -t hotspot-build-opensuse -f scripts/compile-test/OpenSuse .
