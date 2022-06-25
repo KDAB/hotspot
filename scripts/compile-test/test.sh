140aea54bee206ce31926481d0f9eb20e9083216
Milian Wolff <milian.wolff@kdab.com>
2022-06-26 01:13:51 +0200

    Add test presets and use this also for Archlinux

--- scripts/compile-test/test.sh
+++ scripts/compile-test/test.sh
@@ -6,7 +6,8 @@ cd $(dirname $0)/../../
 
 # tst_perfdata is somehow completely broken on this version of ubuntu...
 docker run hotspot-build-ubuntu20.04 bash -c "cd hotspot/build/ && ctest --output-on-failure -j \$(nproc) -E tst_perfdata"
-docker run hotspot-build-ubuntu22.04 bash -c "cd hotspot/build-dev-asan/ && ctest --output-on-failure -j \$(nproc)"
 docker run hotspot-build-fedora bash -c "cd hotspot/build/ && ctest --output-on-failure -j \$(nproc)"
-docker run hotspot-build-archlinux bash -c "cd hotspot/build/ && ctest --output-on-failure -j \$(nproc)"
 docker run hotspot-build-opensuse bash -c "cd hotspot/build/ && ctest --output-on-failure -j \$(nproc)"
+# more modern variants with presets
+docker run hotspot-build-ubuntu22.04 bash -c "cd hotspot && ctest --preset dev-asan -j \$(nproc)"
+docker run hotspot-build-archlinux bash -c "cd hotspot && ctest --preset dev-asan -j \$(nproc)"
