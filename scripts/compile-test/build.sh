04ace93018075a493f1c443de6e8cf669489dc4f
Milian Wolff <milian.wolff@kdab.com>
2022-06-26 15:49:21 +0200

    Give explicit distro version to job names and docker files
    This makes it clear what SpenSuse (Tumbleweed) and Fedora (34)
    we actually test.

--- scripts/compile-test/build.sh
+++ scripts/compile-test/build.sh
@@ -6,6 +6,6 @@ cd $(dirname $0)/../../
 
 docker build -t hotspot-build-ubuntu20.04 -f scripts/compile-test/Ubuntu20.04 .
 docker build -t hotspot-build-ubuntu22.04 -f scripts/compile-test/Ubuntu22.04 .
-docker build -t hotspot-build-fedora -f scripts/compile-test/Fedora .
+docker build -t hotspot-build-fedora34 -f scripts/compile-test/Fedora34 .
 docker build -t hotspot-build-archlinux -f scripts/compile-test/Archlinux .
-docker build -t hotspot-build-opensuse -f scripts/compile-test/OpenSuse .
+docker build -t hotspot-build-opensuse-tumbleweed -f scripts/compile-test/OpenSuseTumbleweed .
