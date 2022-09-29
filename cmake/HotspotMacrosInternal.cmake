99fdd40b9b98e3931ecaa539224d01e32a662b0b
Milian Wolff <milian.wolff@kdab.com>
2022-09-29 22:21:43 +0200

    Enable cmake-lint and cmake-format and reformat build scripts
    While this isn't my old style, it at least makes sure we use a
    consistent style everywhere and don't need to think about it
    anymore in the future.
    
    We don't want to reformat some imported files though, so exclude
    those manually.

--- cmake/HotspotMacrosInternal.cmake
+++ cmake/HotspotMacrosInternal.cmake
@@ -1,16 +1,15 @@
 # Submodule handling
 function(check_submodule_exists name path)
     if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${path}/.git")
-        if (EXISTS "${CMAKE_SOURCE_DIR}/.git")
-            message(FATAL_ERROR
-                "The ${name} git submodule is not initialized.\n"
-                "Please run the following commands in the source directory (${PROJECT_SOURCE_DIR}):\n"
-                "    git submodule update --init --recursive\n"
-                )
-        else()
-            message(FATAL_ERROR
-                "The ${name} submodule is missing - please report a broken source package.\n"
+        if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+            message(
+                FATAL_ERROR
+                    "The ${name} git submodule is not initialized.\n"
+                    "Please run the following commands in the source directory (${PROJECT_SOURCE_DIR}):\n"
+                    "    git submodule update --init --recursive\n"
             )
+        else()
+            message(FATAL_ERROR "The ${name} submodule is missing - please report a broken source package.\n")
         endif()
     endif()
 endfunction()
