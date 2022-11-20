02388c97187b808fbbb7c9f071bd91d4ad50b007
Milian Wolff <milian.wolff@kdab.com>
2022-11-20 21:30:16 +0100

    Split building and publishing docker file
    Makes it easier to build-and-test without directly publishing the
    newly created image.

--- scripts/appimage-baseimage/build_docker.sh
+++ scripts/appimage-baseimage/build_docker.sh
@@ -9,15 +9,3 @@ set -e
 
 cd $(dirname $0)
 docker build --rm -t ghcr.io/kdab/kdesrc-build .
-
-echo "uploading to image to ghcr.io"
-
-ghcr_user=$(pass show ghcr.io | grep 'login: ' | cut -d' ' -f 2)
-pass show ghcr.io | head -n1 | docker login ghcr.io -u $ghcr_user --password-stdin
-
-logout() {
-    docker logout ghcr.io
-}
-trap logout EXIT
-
-docker push ghcr.io/kdab/kdesrc-build:latest
---
+++ scripts/appimage-baseimage/publish_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# SPDX-FileCopyrightText: Milian Wolff <milian.wolff@kdab.com>
+# SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+set -e
+
+echo "uploading docker image to ghcr.io"
+
+ghcr_user=$(pass show ghcr.io | grep 'login: ' | cut -d' ' -f 2)
+pass show ghcr.io | head -n1 | docker login ghcr.io -u $ghcr_user --password-stdin
+
+logout() {
+    docker logout ghcr.io
+}
+trap logout EXIT
+
+docker push ghcr.io/kdab/kdesrc-build:latest
