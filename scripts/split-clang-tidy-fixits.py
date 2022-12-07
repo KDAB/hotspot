458d8cef20d9311fa89e44fbddbcec32b89b3606
Milian Wolff <milian.wolff@kdab.com>
2022-12-07 16:22:23 +0100

    Add script to split monolithic clang-tidy fixits.yaml into chunks
    I.e. group the chunks by diagnostic name such that we can apply them
    individually instead of running one monster patch for everything.

---
+++ scripts/split-clang-tidy-fixits.py
@@ -0,0 +1,45 @@
+#!/bin/env python3
+# SPDX-FileCopyrightText: Milian Wolff <milian.wolff@kdab.com>
+# SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import os
+import sys
+import yaml
+
+
+def fileOffsetToLine(message):
+    ''' to ease manual inspection, translate FileOffset to a FileLine '''
+    if message['FilePath'] == "":
+        return
+    with open(message['FilePath'], 'r', encoding='utf-8') as sourceFile:
+        numNewlines = sourceFile.read(message['FileOffset']).count('\n')
+        message['FileLine'] = numNewlines + 1
+
+
+inputFile = sys.argv[1]
+groupedFixits = {}
+
+
+with open(inputFile, 'r', encoding='utf-8') as mainFixitsFile:
+    mainFixits = yaml.safe_load(mainFixitsFile)
+    for fixit in mainFixits['Diagnostics']:
+        fileOffsetToLine(fixit['DiagnosticMessage'])
+        for note in fixit.get('Notes', []):
+            fileOffsetToLine(note)
+
+        diagnostic = fixit['DiagnosticName']
+        group = groupedFixits.get(diagnostic)
+        if not group:
+            groupedFixits[diagnostic] = [fixit]
+        else:
+            group.append(fixit)
+
+
+baseDir = os.path.dirname(inputFile)
+for diagnostic, fixits in groupedFixits.items():
+    diagnosticDir = f"{baseDir}/{diagnostic}"
+    if not os.path.isdir(diagnosticDir):
+        os.mkdir(diagnosticDir)
+    with open(f"{diagnosticDir}/fixits.yaml", 'w', encoding='utf-8') as fixitsFile:
+        yaml.dump({'Diagnostics': fixits, 'MainSourceFile': ''}, fixitsFile)
