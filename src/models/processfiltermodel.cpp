9b91b8b13dc38c36d29c5ae3975f7cb3df3bcb5d
Allen Winter <allen.winter@kdab.com>
2024-01-16 15:58:39 -0500

    Remove the date range from KDAB copyrights
    Keep the file creationYear as part of the copyright
    but no longer worry about extending the date range
    to include the currentYear.
    
    This standardizes to KDAB copyright policy.

--- src/models/processfiltermodel.cpp
+++ src/models/processfiltermodel.cpp
@@ -1,7 +1,7 @@
 /*
     SPDX-FileCopyrightText: Milian Wolff <milian.wolff@kdab.com>
     SPDX-FileCopyrightText: Nate Rogers <nate.rogers@kdab.com>
-    SPDX-FileCopyrightText: 2016-2022 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+    SPDX-FileCopyrightText: 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
