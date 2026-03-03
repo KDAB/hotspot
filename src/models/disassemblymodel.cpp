27aeedba3045ee72d7f03d49cd8b21037f2fee87
bobsayshilol <bobsayshilol@live.co.uk>
2026-03-03 01:03:46 +0000

    fix: Add 3rd argument to avoid %3 showing in disassembly view
    This has been missing since the line was added, and from the commit
    that introduced it it was likely meant to be the hexdump.

--- src/models/disassemblymodel.cpp
+++ src/models/disassemblymodel.cpp
@@ -136,8 +136,8 @@ QVariant DisassemblyModel::data(const QModelIndex& index, int role) const
             const auto& locationCost = it.value();
 
             if (role == Qt::ToolTipRole) {
-                auto tooltip = tr("addr: <tt>%1</tt><br/>assembly: <tt>%2</tt><br/>disassembly: <tt>%3</tt>")
-                                   .arg(QString::number(data.addr, 16), line);
+                auto tooltip = tr("addr: <tt>%1</tt><br/>assembly: <tt>%2</tt><br/>hexdump: <tt>%3</tt>")
+                                   .arg(QString::number(data.addr, 16), line, data.hexdump);
                 return Util::formatTooltip(tooltip, locationCost, m_results.selfCosts);
             }
 
