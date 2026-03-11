6e38585364ba3b1e5e47c1ec28937fad7ba1c000
Milian Wolff <mail@milianw.de>
2026-03-11 22:00:59 +0100

    fix: remove/cleanup obsolete qt5 docs and scripts
    Remove Ubuntu 22.04, this is too old by now. We have Neon covering
    24.04+KDE and then, new, 25.04 for the future. Will bump that to 26.04
    soon once that's out.
    
    While at it, port from docker to podman as I only have the latter
    nowadays on my system.
    
    Also update Archlinux to kddw 2.3 as 2.2 isn't compatible with newer
    Qt anymore.

--- scripts/compile-test/test.sh
+++ scripts/compile-test/test.sh
@@ -4,30 +4,16 @@ set -e
 
 cd $(dirname $0)/../../
 
-testWithoutPreset()
-{
-    tag=$1
-    shift 1
-    docker run hotspot-$tag bash -c "cd hotspot/build/ && ctest --output-on-failure -j \$(nproc) $@"
-}
-
 testWithPreset()
 {
     tag=$1
     shift 1
 
-    suffix=
-    if [[ $tag == *qt6 ]]; then
-        suffix=-qt6
-    fi
-
-    docker run hotspot-$tag bash -c "cd hotspot && ctest --preset dev-asan$suffix -j \$(nproc) $@"
+    podman run hotspot-$tag bash -c "cd hotspot && ctest --preset dev-asan -j \$(nproc) $@"
 }
 
-testWithoutPreset ubuntu20.04 "-E tst_perfdata"
-
-testWithPreset ubuntu22.04
-testWithPreset archlinuxqt6
-testWithPreset opensusetumbleweedqt6
-testWithPreset fedora42qt6
-testWithPreset neonqt6
+testWithPreset ubuntu25.04
+testWithPreset archlinux
+testWithPreset opensusetumbleweed
+testWithPreset fedora42
+testWithPreset neon
